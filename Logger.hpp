29a54b8e0b226f46e51cf9105b9b3f3f781103b7
SrAxado <sr.axado@gmail.com>
2025-11-17 18:16:16 +0000

    Finished proejct before code review

--- Logger.hpp
+++ Logger.hpp
@@ -6,7 +6,8 @@ using namespace std;
 
 class Logger {
 private:
-
+  // Timer start time
+  static TIME_POINT startTime;
 
 public:
   static void logLocation(const std::string& message,
@@ -14,9 +15,19 @@ public:
                            int line,
                            const std::string& function);
 
+  // Timer functions
   static void startTimer();
   static void stopTimer(const std::string& function);
+  
+  // Logs to the console the compiler info
   static void printCompileInfo();
 };
 
 // Macros for easy usage
+// Log location macro
+#define LOG_LOCATION(msg) Logger::logLocation(msg, __FILE__, __LINE__, __func__)
+// Timer macros
+#define START_TIMER() Logger::startTimer()
+#define STOP_TIMER() Logger::stopTimer(__func__)
+// Compiler info macro
+#define COMPILE_INFO() Logger::printCompileInfo()
